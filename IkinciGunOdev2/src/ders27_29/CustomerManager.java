b8355c28f4fda6e27350918f8b4b89366ccde4f9
tugaysert <mustafatugaysert@gmail.com>
2022-07-21 18:00:48 +0300

    -

---
+++ IkinciGunOdev2/src/ders27_29/CustomerManager.java
@@ -0,0 +1,15 @@
+package ders27_29;
+
+public class CustomerManager {
+    public void add() {
+        System.out.println("musteri eklendi");
+    }
+
+    public void remove() {
+        System.out.println("musteri eklendi");
+    }
+
+    public void update() {
+        System.out.println("musteri eklendi");
+    }
+}
---
+++ IkinciGunOdev2/src/ders31_34/ProductManager.java
@@ -0,0 +1,8 @@
+package ders31_34;
+
+public class ProductManager {
+    public void add(Product product) {
+        System.out.println("Ürün eklendi: " + product.getName());
+    }
+
+}
